4aed3f89d8c261b7927b7bd350d0eb4601b78d66
Моисеев Александр Васильевич <aleksandr.moiseev2@tmk-group.com>
2025-02-10 18:54:59 +0300

    3.6 > Решение задания

---
+++ test_items.py
@@ -0,0 +1,23 @@
+import time
+from selenium.webdriver.common.by import By
+
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+
+def test_shop_language(browser):
+    
+    # Открываем браузер
+    browser.get(link)
+
+    time.sleep(5)
+
+    btn = browser.find_element(By.CSS_SELECTOR, "button.btn-add-to-basket")
+    btn.click()
+
+    assert True, 'Error text'
+
+
+
+    
+
+
