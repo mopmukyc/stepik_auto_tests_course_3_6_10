1df5c77ab184a9c65fe919a53edd374c3a29079a
Моисеев Александр Васильевич <aleksandr.moiseev2@tmk-group.com>
2025-02-11 13:14:30 +0300

    Sleep fix

--- test_items.py
+++ test_items.py
@@ -9,7 +9,7 @@ def test_shop_language(browser):
     # Открываем браузер
     browser.get(link)
 
-    time.sleep(5)
+    time.sleep(30)
 
     btn = browser.find_element(By.CSS_SELECTOR, "button.btn-add-to-basket")
     btn.click()
